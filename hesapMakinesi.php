b5429d655c7f1d6ddb35fc8180005a40bdfc931d
Tibet Çiğdem <63165314+tibetcigdem1057@users.noreply.github.com>
2021-11-08 14:03:16 +0300

    Add files via upload
    Ders içi hesap makinesi aktivitesi

---
+++ hesapMakinesi.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class HesapMakinesi
+{
+    public $sayi1;
+    public $sayi2;
+
+
+    function __construct()
+    {
+        $this->sayi1=$sayi1;
+        $this->sayi2=$sayi2;
+
+    }
+    
+
+    function toplama($sayi1, $sayi2)
+    {
+        return "Toplama İşlemi : " . $sayi1+$sayi2 . "<br>";
+
+    }
+
+    function cikarma($sayi1, $sayi2)
+    {
+        return "Çıkarma İşlemi : " . $sayi1-$sayi2 . "<br>"
+    }
+
+    function bolme($sayi1, $sayi2)
+    {
+        return "Bölme İşlemi : " . $sayi1 / $sayi2 . "<br>"
+    }
+
+    function carpma($sayi1, $sayi2)
+    {
+        return "Çarpma İşlemi : " . $sayi1 * $sayi2 . "<br>"
+    }
+}
+
+$calculator= new HesapMakinesi;
+echo $calculator->cikarma(15,7)
